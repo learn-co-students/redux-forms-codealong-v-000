4d35a37ac71ce12ee076dd336c3d37d49b7736f9
ldgarber <leah.garber88@gmail.com>
2017-05-11 09:56:52 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import CreateTodo from './components/todos/CreateTodo'
 
 class App extends Component {
   render() {
+    const todos = this.props.store.getState().todos; 
     return (
       <div className="App">
         <CreateTodo store={this.props.store} />
+        <ul>
+          {todos.map(function(todo) {
+            return <li>{todo}</li>;   
+            })
+          }
+       </ul>
       </div>
     );
   }
