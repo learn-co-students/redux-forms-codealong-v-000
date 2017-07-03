f697b6e9364f2cb3dd393a22a9dc8d4209d365ec
Sean Clarke <xseanclarke@gmail.com>
2017-07-03 15:09:22 -0400

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class CreateTodo extends Component {
-
   constructor() {
     super();
     this.state = {
       text: '',
-    };
+    }
   }
 
   handleChange(event) {
     this.setState({
       text: event.target.value
-    });
+    })
   }
 
   handleSubmit(event) {
     event.preventDefault();
     this.props.store.dispatch({
       type: 'ADD_TODO',
-      todo: this.state,
+      todo: this.state
     });
   }
 
@@ -29,16 +28,14 @@ class CreateTodo extends Component {
         <form onSubmit={(event) => this.handleSubmit(event)}>
           <p>
             <label>add todo</label>
-            <input
-          type="text"
-          onChange={(event) => this.handleChange(event)} />
+            <input type="text" onChange={(event) => this.handleChange(event)}/>
           </p>
           <input type="submit" />
-       </form>
-       {this.state.text}
-     </div>
-   );
+        </form>
+        {this.state.text}
+      </div>
+    )
   }
-};
+}
 
 export default CreateTodo;
