5f1061cde3e804b0fed2f97d55163ad4bd51d0d9
smithwebtek <brad@smithwebtek.com>
2017-11-15 11:47:11 -0500

    80% there...

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react'
 
 class CreateTodo extends Component {
+  constructor(){
+    super();
+    this.state = {
+      text: '',
+    }
+  }
+  
+  handleChange = (e) => {
+    this.setState({
+      text: e.target.value
+    });
+  }; 
+
+
+handleSubmit= (event)=>{
+  event.preventDefault();
+  this.props.store.dispatch({
+    type: 'ADD_TODO',
+    todo: this.state,
+  });
+}
+
   render() {
+    console.log("this.props at Render() in 'CreateTodo' component:...")
+    console.log(this.props)
+// debugger
+// Our App component passes the store as a prop down to the CreateTodo component. So if you put a debugger right after the line render in CreateTodo, and type in this.props in the console, you can see we have access to the store as one of the props.//
+
     return(
       <div>
-        Create Todo Component
+        <form>
+          <p>
+            <label>add todo</label>
+            <input type="text" onChange={(e)=> this.handleChange(e)} />
+          </p>
+          <input onSubmit={(e)=> this.handleSubmit(e)} type='submit' />
+        </form>
+        {this.state.text}
       </div>
     )
   }
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,5 +1,21 @@
 export default function manageTodo(state = {
   todos: [],
 }, action) {
-  return state;
+
+  switch (action.type){
+    case 'ADD_TODO':
+    let todo = "// <user entered text>"
+    //get user input text
+    //assign to variable 'todo:'
+    //use variable to pass to store
+    //return updated state
+debugger
+    state = state.todos, { 
+        todo: todo,
+      }
+    return state 
+
+    default: 
+    return state;
+  }
 }
