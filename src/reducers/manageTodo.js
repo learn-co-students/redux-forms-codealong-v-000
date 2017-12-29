c565786901ddf50172dacb26c830da8e6a69d9ef
Logan McAnsh <logan@mcan.sh>
2017-12-29 14:47:46 -0500

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,5 +1,15 @@
-export default function manageTodo(state = {
-  todos: [],
-}, action) {
-  return state;
+export default function manageTodo(
+  state = {
+    todos: []
+  },
+  action
+) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        todos: [...state.todos, action.todo.text]
+      };
+    default:
+      return state;
+  }
 }
