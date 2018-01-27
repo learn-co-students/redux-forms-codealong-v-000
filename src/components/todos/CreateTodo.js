48eaaae34dd39d81b74f3a52bc3c6808285500f2
Nicholas D'Amico <nickalan82@icloud.com>
2018-01-27 12:07:00 -0500

    add Redux and React, todo list add and removing items finished

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,10 +1,55 @@
 import React, { Component } from 'react'
 
 class CreateTodo extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+    }
+  }
+
+  handleChange(event) {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_TODO',
+      todo: this.state,
+    })
+    this.setState({
+      text: '',
+    })
+    document.querySelector('input[type="text"]').value = '';
+  }
+
+  handleClick(event) {
+    this.props.store.dispatch({
+      type: 'REMOVE_TODO',
+      todo: event.target.textContent
+    })
+  }
+
+
   render() {
+    const todoList = this.props.store.getState().todos.map(todo => <li onClick={(event) => this.handleClick(event)} key={todo}>{todo}</li>)
     return(
       <div>
-        Create Todo Component
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <p>
+            <label>add ToDo</label><br />
+            <input type="text" onChange={(event) => this.handleChange(event)} />
+          </p>
+          <input type="submit" />
+        </form>
+        {todoList.length > 0 ? 'Click Todo to remove' : null }
+        <ul>
+          {todoList}
+        </ul>
       </div>
     )
   }
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,5 +1,17 @@
 export default function manageTodo(state = {
   todos: [],
 }, action) {
-  return state;
+  switch (action.type) {
+    case 'ADD_TODO':
+      return { todos: state.todos.concat(action.todo.text)}
+    case 'REMOVE_TODO':
+      const indexTodo = state.todos.indexOf(action.todo);
+      return {todos: [
+        ...state.todos.slice(0, indexTodo),
+        ...state.todos.slice(indexTodo + 1)
+      ]}
+
+    default:
+      return state
+  }
 }
