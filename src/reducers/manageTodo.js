21936db72a4b44fc7f5f0a09e9d92757412f591d
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-06-12 15:30:58 -0400

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,5 +1,21 @@
+// TN: THIS IS THE REDUCER, WHREE THE ACTIONS ARE DEFINED.  THE STATE= ON LINE 3
+// IS JUST TO SET A DEFAULT FOR THE STATE
+
 export default function manageTodo(state = {
   todos: [],
 }, action) {
-  return state;
+  switch (action.type) {
+    case 'ADD_TODO':
+      return { todos: state.todos.concat(action.todo.text) };
+
+    default:
+      return state;
+  }
 }
+
+
+// export default function manageTodo(state = {
+//   todos: [],
+// }, action) {
+//   return state;
+// }
