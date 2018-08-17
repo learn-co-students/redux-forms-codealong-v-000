4c45a581b16b26f3c4defb40a1f4b38094f8dcc3
TylerP33 <tyler3321@gmail.com>
2018-08-17 09:15:44 -0500

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,8 +1,25 @@
+// ./src/reducers/manageTodo.js
+ 
 export default function manageTodo(state = {
   todos: [],
 }, action) {
-
-  console.log("reducer received this action:", action);
-
-  return state;
+  switch (action.type) {
+    case 'ADD_TODO':
+ 
+      console.log({ todos: state.todos.concat(action.payload.text) });
+ 
+      return { todos: state.todos.concat(action.payload.text) };
+ 
+    default:
+      return state;
+  }
 }
+// upon submission, we want to dispatch the following action to the store:
+// {
+//   type: 'ADD_TODO',
+//   todo: buygrocieries (or any example)
+// 
+
+// Before starting, this is the concept behind forms in React
+// When the user clicks the submit button, we want to grab that initial state
+// we want to call store.dispatch({type: 'ADD_TODO', todo: this.state})
