11887437550327e05259185ad0efa185f82bfac1
Courtney <chalt0319@gmail.com>
2018-10-19 12:56:34 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import CreateTodo from './components/todos/CreateTodo'
+import ShowTodo from './components/todos/ShowTodo'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <CreateTodo />
+        <ShowTodo />
       </div>
     );
   }
