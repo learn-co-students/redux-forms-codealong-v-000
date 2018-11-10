c7ad0e6e5e1e1e3210b41e9a408032e34b6d10cc
Mathwell <eageeva@hotmail.com>
2018-11-10 14:43:46 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import manageTodo from './reducers/manageTodo';
 import createStore from './createStore';
+import { connect } from 'react-redux';
 
 let store = createStore(manageTodo);
 
