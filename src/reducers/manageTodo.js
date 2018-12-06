7d2c7f56d8d60ffba2845c4e88732f4785b87ff7
theresamorelli <theresa.l.morelli@gmail.com>
2018-12-06 17:45:22 -0500

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,8 +1,14 @@
 export default function manageTodo(state = {
   todos: [],
 }, action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return { todos: [...state.todos, action.payload.text] }
+    default:
+      return state;
+  }
 
-  console.log("reducer received this action:", action);
+  // console.log("reducer received this action:", action);
 
-  return state;
+  // return state;
 }
