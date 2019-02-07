846e748e4235d314d4422a0c5a1138298d198e2f
Micah Shute <micah.shute@gmail.com>
2019-02-06 20:49:47 -0600

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,8 +1,17 @@
 export default function manageTodo(state = {
   todos: [],
-}, action) {
+}, action) {  
 
-  console.log("reducer received this action:", action);
+  switch(action.type){
+
+    case "ADD_TODO":
+      return {
+        todos: [...state.todos, action.payload]
+      }
+    
+    default: 
+      return state
+  }
 
   return state;
 }
