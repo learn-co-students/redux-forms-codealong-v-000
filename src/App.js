a1eaf782847271ad4d6d37ad2409faf7f0bbaa03
JerYOMat <jeremy.emata@gmail.com>
2019-04-02 04:15:27 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React, { Component } from 'react';
-import CreateTodo from './components/todos/CreateTodo'
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+class CreateTodo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+    };
+  }
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.dispatch({ type: 'ADD_TODO', payload: this.state })
+  }
 
-class App extends Component {
   render() {
     return (
-      <div className="App">
-        <CreateTodo />
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <label>add todo </label>
+            <input type="text" onChange={event => this.handleChange(event)} value={this.state.text} />
+          </p>
+          <input type="submit" value="submit" />
+        </form>
+        {this.state.text}
       </div>
-    );
-  }
+    )
+  };
 }
 
-export default App;
+export default connect()(CreateTodo);
