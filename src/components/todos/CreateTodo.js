561e8f30bcbc1fdfb2dbe25cd1e0485a3719e1d7
pracniscate <lorena.giddens@outlook.com>
2019-04-03 07:02:01 -0400

    done

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class CreateTodo extends Component {
+
+  state = {
+    text: ''
+  };
+
+  handleChange = event => {
+    this.setState({
+      text: event.target.value 
+    });
+  };
+
+  // when called, whatever is currently stored in this.state will be sent to the reducer via dispatched action
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.dispatch({ type: 'ADD_TODO', payload: this.state })
+  };
+
   render() {
     return(
       <div>
-        Create Todo Component
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <p>
+            <label>add todo</label>
+            <input 
+              type="text" 
+              onChange={(event) => this.handleChange(event)} 
+              value={this.state.text} />
+          </p>
+          <input type="submit" />
+        </form>
+        {/* {this.state.text} */}
       </div>
-    )
-  }
-}
+    );
+  };
+};
 
-export default CreateTodo;
+// mapDispatchToProps = dispatch => {
+//   return {
+//     addTodo: formData => dispatch({ type: 'ADD_TODO', payload: formData })
+//   };
+// };
+
+// only need to dispatch an action, & not get info from the store, so use null in place of mapStateToProps
+// if not given any arguments, connect will return dispatch as a prop to the component we're wrapping with connect
+export default connect()(CreateTodo);
