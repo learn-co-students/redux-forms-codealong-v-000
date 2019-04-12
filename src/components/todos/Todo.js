63f380dafaa7f31eb9a8f105a2be17e5e426ba80
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-04-12 14:02:58 -0400

    Done.

---
+++ src/components/todos/Todo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Todo extends Component {
+
+  render() {
+    return(
+      <div>
+        <p>{this.text}</p>
+      </div>
+    )
+  };
+};
+
+export default Todo;
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -2,7 +2,14 @@ export default function manageTodo(state = {
   todos: [],
 }, action) {
 
-  console.log("reducer received this action:", action);
+  switch(action.type) {
 
-  return state;
+    case 'ADD_TODO':
+      console.log({ todos: state.todos.concat(action.todo.text) });
+      return { todos: state.todos.concat(action.todo.text) };
+ 
+    default:
+      return state;
+
+  }
 }
