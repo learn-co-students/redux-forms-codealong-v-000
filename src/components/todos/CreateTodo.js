53b97cf0e9553598117629d7bc4ba7af9d0ebac7
amitnyc83 <amit_nyc@live.com>
2019-04-29 12:02:49 -0400

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,51 +1,98 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+// import { connect } from 'react-redux';
+//
+// class CreateTodo extends Component {
+//
+//   constructor(){
+//     super()
+//     this.state = {
+//       text: '',
+//     };
+//   }
+//
+//   handleChange = event => {
+//     this.setState({
+//       text: event.target.value
+//     });
+//   }
+//
+//   handleSubmit = event => {
+//     event.preventDefault();
+//     this.props.addTodo(this.state)
+//   }
+//
+//
+//   render() {
+//     return(
+//       <div>
+//         <form onSubmit={event => this.handleSubmit(event)}>
+//           <p>
+//             <label>add todo</label>
+//             <input
+//               type="text"
+//               onChange={this.handleChange}
+//               value={this.state.text}
+//             />
+//           </p>
+//           <input type="submit" />
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+//
+//
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addToDo: formData => dispatch({type: 'ADD_TODO', payload: formData})
+//   };
+// };
+//
+// export default connect(null, mapDispatchToProps)(CreateTodo);
+
+
+
+
+//Alternate export statement. If no args are given to connect in export, connect will dispatch as a prop to the component we're wrapping
+
+
+
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class CreateTodo extends Component {
+	state = {
+		text: ''
+	};
 
-  constructor(){
-    super()
-    this.state = {
-      text: '',
-    };
-  }
+	handleChange = event => {
+		this.setState({
+			text: event.target.value
+		});
+	};
 
-  handleChange = event => {
-    this.setState({
-      text: event.target.value
-    });
-  }
+	handleSubmit = event => {
+		event.preventDefault();
+		this.props.dispatch({ type: 'ADD_TODO', payload: this.state });
+	};
 
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.addTodo(this.state)
-  }
-
-
-  render() {
-    return(
-      <div>
-        <form onSubmit={event => this.handleSubmit(event)}>
-          <p>
-            <label>add todo</label>
-            <input
-              type="text"
-              onChange={this.handleChange}
-              value={this.state.text}
-            />
-          </p>
-          <input type="submit" />
-        </form>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<form onSubmit={event => this.handleSubmit(event)}>
+					<p>
+						<label>add todo</label>
+						<input
+							type="text"
+							onChange={event => this.handleChange(event)}
+							value={this.state.text}
+						/>
+					</p>
+					<input type="submit" />
+				</form>
+			</div>
+		);
+	}
 }
 
-
-const mapDispatchToProps = dispatch => {
-  return {
-    addToDo: formData => dispatch({type: 'ADD_TODO', payload: formData})
-  };
-};
-
-export default connect(null, mapDispatchToProps)(CreateTodo);
+export default connect()(CreateTodo);
