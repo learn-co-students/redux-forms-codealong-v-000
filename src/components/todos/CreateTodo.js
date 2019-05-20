0a52da0c9829cd7a5e12e3acba432692b0023456
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2019-05-20 07:58:09 -0400

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
 
 class CreateTodo extends Component {
+
+    state = {
+      text: '',
+    };
+
+
+  // using arrow function in the onChange event handler, 'this' will default
+  // to the context they are in.
+  handleChange = event => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addTodo(this.state)
+  }
+
   render() {
     return(
       <div>
-        Create Todo Component
+        <form onSubmit={ event => this.handleSubmit(event) }>
+          <p>
+            <label>add todo</label>
+            <input
+              type="text"
+              onChange={event => this.handleChange(event)}
+              value={this.state.text}/>
+          </p>
+          <input type="submit" />
+        </form>
       </div>
     )
   }
 }
 
-export default CreateTodo;
+const mapDispatchToProps = dispatch => {
+  return {
+    // here, addTodo becomes a function in props that can take arguments
+    // in the component, we can call this.props.addTodo(this.state), because
+    // this.setState is an object with one property only, text.
+    addTodo: formData => dispatch({ type: 'ADD_TODO', payload: formData})
+  }
+}
+
+
+
+
+export default connect(null, mapDispatchToProps)(CreateTodo);
