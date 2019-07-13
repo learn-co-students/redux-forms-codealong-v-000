234560d0f7214e910a5392a8f2fa9b2ee9c335af
NotoriousCottonball <gpianka1@gmail.com>
2019-07-12 22:28:54 -0700

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,80 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+
+/*
+First, we made sure the React component of our application was working. We did this by building a form, and then making sure that whenever the user typed in the form's input, the state was updated.
+
+Second, We connected the component to Redux and designed our mapDispatchToProps
+
+Third, we built our reducer such that it responded to the appropriate event and concatenated the payload into our array of todos.
+*/
 
 class CreateTodo extends Component {
+
+  constructor(props) {
+  super(props);
+  this.handleChange = this.handleChange.bind(this);
+  }
+
+  state = {
+    text: ''
+  }
+
+  handleChange(event) {
+    this.setState({
+      text: event.target.value
+    });
+  };
+
+  /*
+  handleChange = event => {
+    this.setState({
+      text: event.target.value
+    });
+  }
+  */
+
+  handleSubmit = event => {
+        event.preventDefault();
+        this.props.addTodo(this.state);
+    };
+
+    /*
+    Remember that, if not given any arguments, connect will return dispatch as a prop to the component we're wrapping with connect. So an alternative way to write the CreateTodo component could be:
+
+    handleSubmit = event => {
+            event.preventDefault();
+            this.props.dispatch({ type: 'ADD_TODO', payload: this.state });
+        };
+
+    export default connect()(CreateTodo);
+    */
+
   render() {
     return(
       <div>
-        Create Todo Component
+      <form onSubmit={this.handleSubmit}>
+        <p>
+          <label>add todo</label>
+          <input type="text" value={this.state.text} onChange={this.handleChange}/>
+        </p>
+        <input type="submit" />
+      </form>
       </div>
     )
   }
 }
 
-export default CreateTodo;
+
+
+const mapDispatchToProps = dispatch => {
+    return {
+        addTodo: formData => dispatch({ type: 'ADD_TODO', payload: formData })
+    };
+};
+
+/*
+In this component, we're not currently concerned with writing a mapStateToProps function (the first argument passed to connect) as this component doesn't need any state. Since we only need to dispatch an action here and not getting information from our store, we can use null instead of mapStateToProps as the first argument.
+*/
+
+export default connect(null, mapDispatchToProps)(CreateTodo);
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -2,7 +2,21 @@ export default function manageTodo(state = {
   todos: [],
 }, action) {
 
-  console.log("reducer received this action:", action);
+  switch (action.type) {
+    case 'ADD_TODO':
 
-  return state;
+      console.log({ todos: state.todos.concat(action.payload.text) });
+
+      return { todos: state.todos.concat(action.payload.text) };
+
+    default:
+      return state;
+  }
 }
+/*
+First, we made sure the React component of our application was working. We did this by building a form, and then making sure that whenever the user typed in the form's input, the state was updated.
+
+Second, We connected the component to Redux and designed our mapDispatchToProps
+
+Third, we built our reducer such that it responded to the appropriate event and concatenated the payload into our array of todos.
+*/
