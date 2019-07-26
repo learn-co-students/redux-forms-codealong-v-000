4a9ef236e5b69780fedf5097a1ab2d85929c4de3
Ludmilla Aires Negri <ludaires@gmail.com>
2019-07-26 10:50:47 -0400

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class CreateTodo extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+    };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    this.props.dispatch({type: 'ADD_TODO', playload: this.state})
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      text: e.target.value
+    })
+  }
   render() {
     return(
       <div>
-        Create Todo Component
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <p>
+            <label>add todo</label>
+            <input onChange={this.handleChange} value={this.state.text} type="text"/>
+          </p>
+            <input type="submit"/>
+        </form>
+        {this.state.text}
       </div>
     )
   }
 }
 
-export default CreateTodo;
+
+
+
+export default connect()(CreateTodo);
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,8 +1,12 @@
 export default function manageTodo(state = {
   todos: [],
 }, action) {
-
-  console.log("reducer received this action:", action);
-
-  return state;
+  switch (action.type) {
+    case 'ADD_TODO':
+      console.log({todos: state.todos.concat(action.playload.text) })
+      return {todos: state.todos.concat(action.playload.text )}
+    
+    default:
+      return state;
+  }
 }
