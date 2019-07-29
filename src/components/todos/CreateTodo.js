7c0010c0635c011a1e474f773307c2631b24d1b3
MargaritaKhodzka <mkhodzka@gmail.com>
2019-07-29 14:21:29 -0400

    create controlled form using state and onChange event handler

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 
 class CreateTodo extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      text: ''
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      text: event.target.value
+    });
+  };
+
   render() {
     return(
       <div>
         <form>
           <p>
             <label>add todo</label>
-            <input type = 'text' />
+            <input type = 'text' onChange={this.handleChange} value={this.state.text}/>
           </p>
           <input type = 'submit' />
         </form>
+        {this.state.text}
       </div>
     );
   }
