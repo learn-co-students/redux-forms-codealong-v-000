eea53787bca9d8a4ccea962cec4e868bdcaa6446
MargaritaKhodzka <mkhodzka@gmail.com>
2019-07-29 14:35:00 -0400

    dispatch an action to the store on submit

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class CreateTodo extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      text: ''
-    };
-  }
+  state = {
+    text: ''
+  };
 
   handleChange = event => {
     this.setState({
@@ -15,20 +13,30 @@ class CreateTodo extends Component {
     });
   };
 
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.dispatch({
+      type: 'ADD_TODO',
+      payload: this.state
+    });
+  };
+
   render() {
     return(
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <p>
             <label>add todo</label>
-            <input type = 'text' onChange={this.handleChange} value={this.state.text}/>
+            <input
+              type = 'text'
+              onChange={this.handleChange}
+              value={this.state.text}/>
           </p>
           <input type = 'submit' />
         </form>
-        {this.state.text}
       </div>
     );
   }
 };
 
-export default CreateTodo;
+export default connect()(CreateTodo);
