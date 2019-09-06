7b794ccf2f0ceef68d44cf5f38d5d0264c4623f4
cbshields <cbshields@outlook.com>
2019-09-06 10:56:51 -0700

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class CreateTodo extends Component {
+    state = {
+      text: ''
+    }
+
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  // handleSubmit = event => {
+  //       event.preventDefault();
+  //       this.props.addTodo(this.state);
+  //   };
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.dispatch({type: 'ADD_TODO', payload: this.state})
+  }
+
   render() {
     return(
       <div>
-        Create Todo Component
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <p>
+            <label>add todo</label>
+            <input type="text"
+              onChange={this.handleChange}
+              value={this.state.text}
+            />
+          </p>
+            <input type="submit"/>
+        </form>
+          {this.state.text}
       </div>
     )
   }
 }
 
-export default CreateTodo;
+// const mapDispatchToProps = dispatch => {
+//   return {
+//       addTodo: formData => dispatch({type: 'ADD_TODO', payload: formData})
+//   }
+// }
+
+// export default connect(null, mapDispatchToProps)(CreateTodo);
+
+// if there is no parameter listed for connect, it automatically dispatches to the Store
+// you need to make sure your handleSubmit function passes that dispatch params
+export default connect()(CreateTodo)
