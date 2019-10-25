2f02266d9011c395213d43bb1f8c49fcbec45a7b
Sarah Paz <sarah.paz@gmail.com>
2019-10-25 18:36:26 -0400

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class CreateTodo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: ""
+    };
+  }
+
+  handleChange = e => {
+    //* arrow functions don't define their own version of .this, they default to the context they are in.
+    this.setState({
+      text: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault(e);
+    this.props.dispatch({ type: "ADD_TODO", payload: this.state });
+  };
+
   render() {
-    return(
+    return (
       <div>
-        Create Todo Component
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <p>
+            <label>add todo</label>
+            <input
+              type="text"
+              onChange={e => this.handleChange(e)}
+              value={this.state.text}
+            />
+          </p>
+          <input type="submit" />
+        </form>
+        {this.state.text}
       </div>
-    )
+    );
   }
 }
 
-export default CreateTodo;
+export default connect()(CreateTodo);
