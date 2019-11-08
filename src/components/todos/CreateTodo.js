13f21f155da0452bf354b00dca11a02e3d756ffc
epic0448 <samuelogundiran@Yahoo.com>
2019-11-08 15:20:54 -0500

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,59 @@
 import React, { Component } from 'react'
+import { throws } from 'assert'
+import { connect } from 'react-redux'
+
+// NOTE: there is an alternative was to write this.
 
 class CreateTodo extends Component {
+
+     state = {
+      text: '',
+    };
+  
+  //  To keep handleChange to function only instance of the components it is defined in.
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addTodo(this.state)
+   }
+  
+
   render() {
     return(
       <div>
-        Create Todo Component
+        <form onSubmit={ event => this.handleSubmit(event) }> 
+          <p>
+            <label>add todo</label>
+            {/* When ever the input changes, we want to change the state
+             by adding an onChange event listner here. */}
+            <input type="text" onChange={this.handleChange} value={this.state.text}/>
+          </p>
+          <input type="submit" />
+        </form>
+        {this.state.text}
       </div>
     )
   }
 }
 
-export default CreateTodo;
+const mapDispatchToProps = dispatch => {
+  return {
+    addTodo: formData => dispatch({ type: 'ADD_TODO', payload: formData })
+
+    // {/* 
+    //   NOTE: payLOad is small letter.
+    // On submission of the form in our component, 
+    // we want to send the value we've captured in the local state to be added to our Redux store. 
+    // With the above set up, addTodo becomes a function in
+    //  props that is able take arguments.
+    // */}
+  }
+}
+
+
+export default connect (null, mapDispatchToProps)(CreateTodo);
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,8 +1,23 @@
-export default function manageTodo(state = {
-  todos: [],
-}, action) {
-
-  console.log("reducer received this action:", action);
-
-  return state;
+export default function manageTodo(
+    state = {
+     todos: [],
+     }, action) {
+     switch (action.type) {
+         case 'ADD_TODO':
+      
+      console.log({ todos:
+          state.todos.concat(action.payload.text)
+      });
+      return { todos: state.todos.concat(action.payload.text) };
+     
+    default:
+      return state;
+  }
 }
+
+ /*
+ Ok, once you change the manageTodo() reducer to the above function,
+  open up the console in your browser, and try clicking the submit button a few times.
+  The log will show that our reducer is concatenating new
+   values every time the form is submitted!
+ */
