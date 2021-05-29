de6888225a4f600e99f6266ea6258dd88e009fbd
guysbryant <guy.s.bryant@gmail.com>
2021-05-29 11:22:52 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import CreateTodo from './components/todos/CreateTodo'
+import Todos from './components/todos/Todos'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <CreateTodo />
+        <Todos />
       </div>
     );
   }
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -3,6 +3,14 @@ export default function manageTodo(state = {
 }, action) {
 
   console.log("reducer received this action:", action);
+    switch(action.type){
+    case 'ADD_TODO':
+        console.log(state.todos)
+        return{
+            todos: [ ...state.todos, action.payload.text ]
+        }
+    default:
+        return state
+    }
 
-  return state;
 }
