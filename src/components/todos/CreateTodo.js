03ad3785c93d40e7f8f13453ba43448b8869fef4
github username <github email address>
2021-06-07 18:59:33 -0400

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
 
 class CreateTodo extends Component {
+  state = {
+    text: ""
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({
+      text: e.target.value
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addTodo(this.state);
+  };
+
   render() {
     return(
       <div>
-        Create Todo Component
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <label>add todo</label>
+            <input type="text" onChange={this.handleChange} value={this.state.text}/>
+          </p>
+          <input type="submit" />
+        </form>
+        {this.state.text}
       </div>
     )
   }
 }
 
-export default CreateTodo;
+const mapDispatchToProps = dispatch => ({
+	addTodo: formData => dispatch({ type: 'ADD_TODO', payload: formData })
+});
+
+//On submission of the form in our component, 
+//we want to send the value we've captured in the local state
+//to be added to our Redux store. 
+
+export default connect(null, mapDispatchToProps)(CreateTodo);
