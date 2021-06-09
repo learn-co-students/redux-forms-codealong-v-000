1e278a134e8cfb90b35e849d70c28ce1c536a98b
Laura Travin <laura.travin@gmail.com>
2021-06-09 16:45:43 -0400

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -3,6 +3,16 @@ export default function manageTodo(state = {
 }, action) {
 
   console.log("reducer received this action:", action);
+  switch (action.type){
+    case 'ADD_TODO':
+    //  console.log({ todos: state.todos.concat(action.payload.text) });
+      console.log(action);
+
+      console.log(state.todos);
+      return { todos: state.todos.concat(action.payload.text) };
+ 
+  
 
   return state;
+ } 
 }
